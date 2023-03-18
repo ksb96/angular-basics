d58fda9d107b456bc140dfe14e806211163077e2
kubstar <52975077+kubstar@users.noreply.github.com>
2023-03-18 12:08:49 +0530

    comments -A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // add inline <html> !recommended
   // template: `<p>Hello world!</p>`,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  // add multiple styles
   // styles: ['']
 })
 export class AppComponent {
